539e99e4715756e7062393bb238123163b504d90
sherigr <SGrossman00@gmail.com>
2017-05-18 15:54:19 -0400

    first commit

---
+++ recursively.js
@@ -0,0 +1,62 @@
+
+var canine = {
+  bark: function() {
+    console.log('bark');
+  }
+};
+
+var dog = Object.create(canine);
+
+dog.fetch = function() {
+  console.log('fetch');
+};
+
+var myDog = Object.create(dog);
+var empty = Object.create(null);
+
+
+function isPrototypeOf(proto, obj) {
+
+  if(Object.getPrototypeOf(obj) === proto) {
+    return true;
+  }
+  // Object.create(null) does not inherit from Object.prototype
+  else if(Object.getPrototypeOf(obj) === null || Object.getPrototypeOf(obj) === undefined) {
+    return false;
+  }
+  else {
+    return isPrototypeOf(proto, Object.getPrototypeOf(obj));
+  }
+}
+
+tests({
+  'Dog is the prototype of myDog': function() {
+  // dog.isPrototypeOf(myDog);   native function => true
+  // isPrototypeOf(dog, myDog);  function does the same
+  eq(isPrototypeOf(dog, myDog), true);
+  },
+  'Canine is the prototype of dog': function() {
+  // canine.isPrototypeOf(dog) => true
+  eq(isPrototypeOf(canine, dog), true);
+  },
+  'A new object created from the prototype should not be the prototype of the prototye. Therefore, myDog should not be the prototype of dog': function() {
+  // myDog.isPrototypeOf(dog) returns false
+  eq(isPrototypeOf(myDog, dog), false);
+  },
+  'Object.create(null) does not inherit from Object.prototype so dog should not be the prototype of empty': function() {
+  // dog.isPrototypeOf(empty);  => false
+  eq(isPrototypeOf(dog, empty), false);
+  },
+  'Object.create(null) does not inherit from Object.prototype so canine should not be the prototype of empty': function() {
+  eq(isPrototypeOf(canine, empty), false);
+  },
+  'Passing in Object.prototype as the prototype argument should return true if it is the prototype of the object': function() {
+  // Object.prototype.isPrototypeOf(myDog);  => true// your function does the same
+  eq(isPrototypeOf(Object.prototype, myDog), true);
+  },
+  'The function will work for any number of prototype links passed in': function() {
+  // Function will work for any number of prototype links.
+  eq(isPrototypeOf(canine, myDog), true) // true  
+  }    
+});
+
